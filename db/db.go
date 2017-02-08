06c3be64976e4ff18c1097dbc1bb83f33ff66c97
Sam Kleinman <sam@10gen.com>
2017-02-08 18:09:05 -0500

    wrap errors

--- db/db.go
+++ db/db.go
@@ -1,7 +1,8 @@
 package db
 
 import (
-	mgo "gopkg.in/mgo.v2"
+	"github.com/pkg/errors"
+	"github.com/tychoish/sink"
 )
 
 func Insert(collection string, item interface{}) error {
@@ -10,23 +11,23 @@ func Insert(collection string, item interface{}) error {
 		return err
 	}
 	defer session.Close()
-	return db.C(collection).Insert(item)
+
+	return errors.Wrap(db.C(collection).Insert(item), "problem inserting restul")
 }
 
 // FindOne finds one item from the specified collection and unmarshals it into the
 // provided interface, which must be a pointer.
-func FindOne(collection string, query interface{},
-	projection interface{}, sort []string, out interface{}) error {
-
+func FindOne(coll string, query interface{}, proj interface{}, sort []string, out interface{}) error {
 	session, db, err := sink.GetMgoSession()
 	if err != nil {
-		return err
+		return errors.Wrap(err, "problem getting session")
 	}
 	defer session.Close()
 
-	q := db.C(collection).Find(query).Select(projection)
+	q := db.C(coll).Find(query).Select(proj)
 	if len(sort) != 0 {
 		q = q.Sort(sort...)
 	}
-	return q.One(out)
+
+	return errors.Wrap(q.One(out), "problem resolving results")
 }
