b8840c224ca43ccb104cc50c9632b1bf8aef615c
Sam Kleinman <sam@10gen.com>
2018-08-24 11:17:00 -0400

    add test to check remote jobs

---
+++ units/units_test.go
@@ -0,0 +1,37 @@
+package units
+
+import (
+	"testing"
+
+	"github.com/mongodb/amboy"
+	"github.com/mongodb/amboy/registry"
+	"github.com/mongodb/grip"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAllRegisteredUnitsAreRemoteSafe(t *testing.T) {
+	assert := assert.New(t)
+
+	for id := range registry.JobTypeNames() {
+		grip.Infoln("testing job is remote ready:", id)
+		factory, err := registry.GetJobFactory(id)
+		assert.NoError(err)
+		assert.NotNil(factory)
+		job := factory()
+
+		assert.NotNil(job)
+
+		assert.Equal(id, job.Type().Name)
+
+		for _, f := range []amboy.Format{amboy.JSON, amboy.YAML, amboy.JSON} {
+			assert.NotPanics(func() {
+				dbjob, err := registry.MakeJobInterchange(job, f)
+
+				assert.NoError(err)
+				assert.NotNil(dbjob)
+				assert.NotNil(dbjob.Dependency)
+				assert.Equal(id, dbjob.Type)
+			}, id)
+		}
+	}
+}
