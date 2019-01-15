02869f2db9e5da785fb148bf2c19909293f89815
julianedwards <julian.edwards@mongodb.com>
2019-01-15 14:34:01 -0500

    vendor aviation

---
+++ vendor/github.com/evergreen-ci/aviation/context.go
@@ -0,0 +1,50 @@
+package aviation
+
+import (
+	"context"
+	"time"
+)
+
+type contextKey int
+
+const (
+	requestIDKey contextKey = iota
+	startAtKey
+	userKey
+)
+
+func SetRequestStartAt(ctx context.Context, startAt time.Time) context.Context {
+	return context.WithValue(ctx, startAtKey, startAt)
+}
+
+func GetRequestStartAt(ctx context.Context) time.Time {
+	if rv := ctx.Value(startAtKey); rv != nil {
+		if t, ok := rv.(time.Time); ok {
+			return t
+		}
+	}
+
+	return time.Time{}
+}
+
+// SetRequestUser adds a user to a context. This function is public to
+// support teasing workflows.
+func SetRequestUser(ctx context.Context, u User) context.Context {
+	return context.WithValue(ctx, userKey, u)
+}
+
+// GetUser returns the user attached to the request. The User object
+// is nil when
+func GetUser(ctx context.Context) User {
+	u := ctx.Value(userKey)
+	if u == nil {
+		return nil
+	}
+
+	usr, ok := u.(User)
+	if !ok {
+		return nil
+	}
+
+	return usr
+}
