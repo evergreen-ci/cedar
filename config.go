9e0c571428f3dc7b237bae43a855947c82693cdd
julianedwards <julian.edwards@mongodb.com>
2019-01-15 15:22:47 -0500

    add user manager to cedar config

--- config.go
+++ config.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"time"
 
+	"github.com/evergreen-ci/gimlet"
 	"github.com/mongodb/grip"
 )
 
@@ -15,21 +16,25 @@ type Configuration struct {
 	MongoDBDialTimeout time.Duration
 	UseLocalQueue      bool
 	NumWorkers         int
+	UserManager        gimlet.UserManager
+	ExpireAfter        time.Duration
 }
 
 func (c *Configuration) Validate() error {
 	catcher := grip.NewBasicCatcher()
+
 	if c.MongoDBURI == "" {
 		catcher.Add(errors.New("must specify a mongodb url"))
 	}
-
 	if c.NumWorkers < 1 {
 		catcher.Add(errors.New("must specify a valid number of amboy workers"))
 	}
-
 	if c.MongoDBDialTimeout <= 0 {
 		c.MongoDBDialTimeout = 2 * time.Second
-
 	}
+	if c.ExpireAfter <= 0 {
+		c.ExpireAfter = time.Duration(365*24*60) * time.Minute
+	}
+
 	return catcher.Resolve()
 }
