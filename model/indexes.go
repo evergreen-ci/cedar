cf9b31bc15c84cd32207eaeb7700fb766e774ea6
julianedwards <julian.edwards@mongodb.com>
2019-01-17 16:31:52 -0500

    cr

--- model/indexes.go
+++ model/indexes.go
@@ -1,52 +1,88 @@
-package model
-
 // +build ignore
+
+package model
 
 import (
 	"github.com/mongodb/anser/bsonutil"
 	"gopkg.in/mgo.v2/bson"
 )
 
-// Indexes for "perf_results" collection.
-const (
-	PerfResultsCreatedCompletedIndex = bson.D{{perfCreatedAtKey, 1}, {perfCompletedAtKey, 1}}
-	PerfResultsProjectIndex          = bson.D{
-		{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoProjectKey), 1},
-	}
-	PerfResultsVersionIndex = bson.D{
-		{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoVersionKey), 1},
-	}
-	PerfResultsVariantIndex = bson.D{
-		{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoVariantKey), 1},
-	}
-	PerfResultsTaskNameIndex = bson.D{
-		{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoTaskNameKey), 1},
-	}
-	PerfResultsTaskIDIndex = bson.D{
-		{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoTaskIDKey), 1},
-	}
-	PerfResultsExecutionIndex = bson.D{
-		{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoExecutionKey), 1},
-	}
-	PerfResultsTestNameIndex = bson.D{
-		{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoTestNameKey), 1},
-	}
-	PerfResultsTrialIndex = bson.D{
-		{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoTrialKey), 1},
-	}
-	PerfResultsParentIndex = bson.D{
-		{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoParentKey), 1},
-	}
-	PerfResultsTagsIndex = bson.D{
-		{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoTagsKey), 1},
-	}
-	PerfResultsSchemaIndex = bson.D{
-		{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoSchemaKey), 1},
-	}
-)
+// SystemIndexes holds the keys, options and the collection for an index.
+// See
+// https://docs.mongodb.com/manual/reference/method/db.collection.createIndex
+// for more info.
+type SystemIndexes struct {
+	Keys       bson.D
+	Options    bson.D
+	Collection string
+}
 
-// Indexes for "users" collection.
-const (
-	UsersTokenIndex  = bson.D{{bsonutil.GetDottedKeyName(dbUserLoginCacheKey, loginCacheTokenKey), 1}}
-	UsersAPIKeyIndex = bson.D{{dbUserAPIKeyKey, 1}}
-)
+// GetPerfResultsCollectionIndexes returns indexes for the "perf_results"
+// collection.
+func GetPerfResultCollectionIndexes() []SystemIndexes {
+	return []SystemIndexes{
+		{
+			Keys:       bson.D{{perfCreatedAtKey, 1}, {perfCompletedAtKey, 1}},
+			Collection: perfResultCollection,
+		},
+		{
+			Keys:       bson.D{{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoProjectKey), 1}},
+			Collection: perfResultCollection,
+		},
+		{
+			Keys:       bson.D{{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoVersionKey), 1}},
+			Collection: perfResultCollection,
+		},
+		{
+			Keys:       bson.D{{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoVariantKey), 1}},
+			Collection: perfResultCollection,
+		},
+		{
+			Keys:       bson.D{{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoTaskNameKey), 1}},
+			Collection: perfResultCollection,
+		},
+		{
+			Keys:       bson.D{{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoTaskIDKey), 1}},
+			Collection: perfResultCollection,
+		},
+		{
+			Keys:       bson.D{{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoExecutionKey), 1}},
+			Collection: perfResultCollection,
+		},
+		{
+			Keys:       bson.D{{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoTestNameKey), 1}},
+			Collection: perfResultCollection,
+		},
+		{
+			Keys:       bson.D{{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoTrialKey), 1}},
+			Collection: perfResultCollection,
+		},
+		{
+			Keys:       bson.D{{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoParentKey), 1}},
+			Collection: perfResultCollection,
+		},
+		{
+			Keys:       bson.D{{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoTagsKey), 1}},
+			Collection: perfResultCollection,
+		},
+		{
+			Keys:       bson.D{{bsonutil.GetDottedKeyName(perfInfoKey, perfResultInfoSchemaKey), 1}},
+			Collection: perfResultCollection,
+		},
+	}
+}
+
+// GetUserCollectionIndex returns indexes for the "users" collection.
+func GetUserCollectionIndex() []SystemIndexes {
+	return []SystemIndexes{
+		{
+			Keys:       bson.D{{bsonutil.GetDottedKeyName(dbUserLoginCacheKey, loginCacheTokenKey), 1}},
+			Options:    bson.D{{"unique": true}},
+			Collection: userCollection,
+		},
+		{
+			Keys:       bson.D{{dbUserAPIKeyKey, 1}},
+			Collection: userCollection,
+		},
+	}
+}
