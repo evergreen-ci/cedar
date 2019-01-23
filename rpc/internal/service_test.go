e536b0d157bba10745ca2c5d75ac2032264f4176
julianedwards <julian.edwards@mongodb.com>
2019-01-23 17:56:36 -0500

    check stat

--- rpc/internal/service_test.go
+++ rpc/internal/service_test.go
@@ -340,6 +340,10 @@ func TestService(t *testing.T) {
 		for _, file := range files {
 			fmt.Println(file.Name())
 		}
+		err = os.Stat("curator")
+		fmt.Println(err)
+		err = os.Stat("service.go")
+		fmt.Println(err)
 		curatorPath, err := filepath.Abs("curator")
 		require.NoError(t, err)
 		cmd := exec.Command(
