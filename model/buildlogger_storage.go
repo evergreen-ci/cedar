e01c7544ff4c0eba3fb6f37d3effe03750803e12
julianedwards <julian.edwards@mongodb.com>
2019-06-06 14:40:01 -0400

    use pail s3 permissions constants

--- model/buildlogger_storage.go
+++ model/buildlogger_storage.go
@@ -1,36 +1,10 @@
 package model
 
 import (
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/evergreen-ci/pail"
 	"github.com/mongodb/anser/bsonutil"
 	"github.com/pkg/errors"
 )
-
-// S3 Permissions is a type that describes the object canned ACL from S3.
-type S3Permissions string
-
-const (
-	S3PermissionsPrivate                S3Permissions = s3.ObjectCannedACLPrivate
-	S3PermissionsPublicRead             S3Permissions = s3.ObjectCannedACLPublicRead
-	S3PermissionsPublicReadWrite        S3Permissions = s3.ObjectCannedACLPublicReadWrite
-	S3PermissionsAuthenticatedRead      S3Permissions = s3.ObjectCannedACLAuthenticatedRead
-	S3PermissionsAWSExecRead            S3Permissions = s3.ObjectCannedACLAwsExecRead
-	S3PermissionsBucketOwnerRead        S3Permissions = s3.ObjectCannedACLBucketOwnerRead
-	S3PermissionsBucketOwnerFullControl S3Permissions = s3.ObjectCannedACLBucketOwnerFullControl
-)
-
-func (p S3Permissions) Validate() error {
-	switch p {
-	case S3PermissionsPublicRead, S3PermissionsPublicReadWrite:
-		return nil
-	case S3PermissionsPrivate, S3PermissionsAuthenticatedRead, S3PermissionsAWSExecRead:
-		return nil
-	case S3PermissionsBucketOwnerRead, S3PermissionsBucketOwnerFullControl:
-		return nil
-	default:
-		return errors.New("invalid S3 permissions type specified")
-	}
-}
 
 type LogFormat string
 
@@ -55,9 +29,9 @@ func (lf LogFormat) Validate() error {
 // metadata storage. The prefix field indicates the name of the sub-bucket. The
 // top level bucket is accesible via the cedar.Environment interface.
 type LogArtifactInfo struct {
-	Prefix      string        `bson:"prefix"`
-	Permissions S3Permissions `bson:"permissions"`
-	Version     int           `bson:"version"`
+	Prefix      string             `bson:"prefix"`
+	Permissions pail.S3Permissions `bson:"permissions"`
+	Version     int                `bson:"version"`
 }
 
 var (
