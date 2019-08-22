8ddd3495ce912f6df924cb9161932f63d92d970c
Julian Edwards <julian.edwards@mongodb.com>
2019-08-22 18:18:19 -0400

    tests

--- rest/model/buildlogger.go
+++ rest/model/buildlogger.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	dbmodel "github.com/evergreen-ci/cedar/model"
+	"github.com/pkg/errors"
+)
+
 // APILog describes metadata for a buildlogger log.
 type APILog struct {
 	ID          APIString          `json:"id,omitempty"`
@@ -7,6 +12,20 @@ type APILog struct {
 	CreatedAt   APITime            `json:"created_at"`
 	CompletedAt APITime            `json:"completed_at"`
 	Artifact    APILogArtifactInfo `json:"artifact"`
+}
+
+func (apiResult *APILog) Import(i interface{}) error {
+	switch l := i.(type) {
+	case dbmodel.Log:
+		apiResult.ID = ToAPIString(l.ID)
+		apiResult.Info = getLogInfo(l.Info)
+		apiResult.CreatedAt = NewTime(l.CreatedAt)
+		apiResult.CompletedAt = NewTime(l.CompletedAt)
+		apiResult.Artifact = getLogArtifactInfo(l.Artifact)
+	default:
+		return errors.New("incorrect type when fetching converting Log type")
+	}
+	return nil
 }
 
 // APILogInfo describes information unique to a single buildlogger log.
@@ -20,9 +39,67 @@ type APILogInfo struct {
 	TestName    APIString         `bson:"test_name,omitempty"`
 	Trial       int               `bson:"trial"`
 	ProcessName APIString         `bson:"proc_name,omitempty"`
-	Format      LogFormat         `bson:"format,omitempty"`
+	Format      APIString         `bson:"format,omitempty"`
 	Arguments   map[string]string `bson:"args,omitempty"`
 	ExitCode    int               `bson:"exit_code, omitempty"`
-	Mainline    bool              `bson:"mainline"`
-	Schema      int               `bson:"schema,omitempty"`
+}
+
+func getLogInfo(l dbmodel.LogInfo) APILogInfo {
+	return APILogInfo{
+		Project:     ToAPIString(l.Project),
+		Version:     ToAPIString(l.Version),
+		Variant:     ToAPIString(l.Variant),
+		TaskName:    ToAPIString(l.TaskName),
+		TaskID:      ToAPIString(l.TaskID),
+		Execution:   l.Execution,
+		TestName:    ToAPIString(l.TestName),
+		Trial:       l.Trial,
+		ProcessName: ToAPIString(l.ProcessName),
+		Format:      ToAPIString(string(l.Format)),
+		Arguments:   l.Arguments,
+		ExitCode:    l.ExitCode,
+	}
+}
+
+// APILogArtifact describes a bucket of logs stored in some kind of offline
+// blob storage. It is the bridge between pail-backed offline log storage and
+// the cedar-based log metadata storage. The prefix field indicates the name of
+// the "sub-bucket".
+type APILogArtifactInfo struct {
+	Type    APIString         `bson:"type"`
+	Prefix  APIString         `bson:"prefix"`
+	Version int               `bson:"version"`
+	Chunks  []APILogChunkInfo `bson:"chunks,omitempty"`
+}
+
+func getLogArtifactInfo(l dbmodel.LogArtifactInfo) APILogArtifactInfo {
+	chunks := make([]APILogChunkInfo, len(l.Chunks))
+	for i, chunk := range l.Chunks {
+		chunks[i] = getLogChunkInfo(chunk)
+	}
+
+	return APILogArtifactInfo{
+		Type:    ToAPIString(string(l.Type)),
+		Prefix:  ToAPIString(l.Prefix),
+		Version: l.Version,
+		Chunks:  chunks,
+	}
+}
+
+// APILogChunkInfo describes a chunk of log lines stored in pail-backed offline
+// storage.
+type APILogChunkInfo struct {
+	Key      APIString `bson:"key"`
+	NumLines int       `bson:"num_lines"`
+	Start    APITime   `bson:"start"`
+	End      APITime   `bson:"end"`
+}
+
+func getLogChunkInfo(l dbmodel.LogChunkInfo) APILogChunkInfo {
+	return APILogChunkInfo{
+		Key:      ToAPIString(l.Key),
+		NumLines: l.NumLines,
+		Start:    NewTime(l.Start),
+		End:      NewTime(l.End),
+	}
 }
