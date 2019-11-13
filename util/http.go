870a477be0671b30306374f236cd5ef047e6edde
Sam Kleinman <sam@10gen.com>
2019-11-13 17:00:16 -0500

    MAKE-1021: http client pool

---
+++ util/http.go
@@ -0,0 +1,52 @@
+package util
+
+import (
+	"crypto/tls"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+)
+
+const httpClientTimeout = 5 * time.Minute
+
+var httpClientPool *sync.Pool
+
+func init() {
+	httpClientPool = &sync.Pool{
+		New: func() interface{} { return newBaseConfiguredHttpClient() },
+	}
+
+}
+func newConfiguredBaseTransport() *http.Transport {
+	return &http.Transport{
+		TLSClientConfig:     &tls.Config{},
+		Proxy:               http.ProxyFromEnvironment,
+		DisableCompression:  false,
+		DisableKeepAlives:   true,
+		IdleConnTimeout:     20 * time.Second,
+		MaxIdleConnsPerHost: 10,
+		MaxIdleConns:        50,
+		Dial: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 0,
+		}).Dial,
+		TLSHandshakeTimeout: 10 * time.Second,
+	}
+
+}
+
+func GetHTTPClient() *http.Client { return httpClientPool.Get().(*http.Client) }
+
+func PutHTTPClient(c *http.Client) {
+	c.Timeout = httpClientTimeout
+
+	switch c.Transport.(type) {
+	case *http.Transport:
+		c.Transport.(*http.Transport).TLSClientConfig.InsecureSkipVerify = false
+		httpClientPool.Put(c)
+	default:
+		c.Transport = newConfiguredBaseTransport()
+		httpClientPool.Put(c)
+	}
+}
