5d683c2540a78bbcb1363b1d682a8d7987ce62e5
julianedwards <julian.edwards@mongodb.com>
2019-12-09 17:28:12 -0500

    add retries with backoff

--- rest/auth.go
+++ rest/auth.go
@@ -7,12 +7,13 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/evergreen-ci/cedar/util"
 	"github.com/evergreen-ci/gimlet"
+	"github.com/jpillora/backoff"
 	"github.com/pkg/errors"
 )
 
 func evergreenProxyAuthLogRead(ctx context.Context, userToken, baseURL, resourceId string) gimlet.Responder {
-	client := &http.Client{Timeout: time.Minute}
 	urlString := fmt.Sprintf("%s?resource=%s&resource_type=project&permission=project_logs&required_level=10", baseURL, resourceId)
 	req, err := http.NewRequest(http.MethodGet, urlString, nil)
 	if err != nil {
@@ -21,28 +22,44 @@ func evergreenProxyAuthLogRead(ctx context.Context, userToken, baseURL, resource
 	req = req.WithContext(ctx)
 	req.Header = map[string][]string{"mci-token": {userToken}}
 
-	resp, err := client.Do(req)
-	if err != nil {
-		return gimlet.MakeJSONInternalErrorResponder(errors.Wrap(err, "error authenticating user"))
+	client := util.GetHTTPClient()
+	defer util.PutHTTPClient(client)
+
+	b := &backoff.Backoff{
+		Min:    100 * time.Millisecond,
+		Max:    10 * time.Second,
+		Factor: 2,
 	}
-	if resp.StatusCode == http.StatusUnauthorized {
-		return gimlet.MakeTextErrorResponder(gimlet.ErrorResponse{
-			StatusCode: http.StatusUnauthorized,
-			Message:    "unauthorized user",
-		})
+	var responder gimlet.Responder
+	for i := 0; i < 10; i++ {
+		resp, err := client.Do(req)
+		if err != nil {
+			responder = gimlet.MakeJSONInternalErrorResponder(errors.Wrap(err, "error authenticating user"))
+			time.Sleep(b.Duration())
+			continue
+		}
+		if resp.StatusCode == http.StatusUnauthorized {
+			return gimlet.MakeTextErrorResponder(gimlet.ErrorResponse{
+				StatusCode: http.StatusUnauthorized,
+				Message:    "unauthorized user",
+			})
+		}
+
+		bytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			responder = gimlet.MakeJSONInternalErrorResponder(errors.Wrap(err, "error reading response body"))
+			time.Sleep(b.Duration())
+			continue
+		}
+
+		if string(bytes) != "true" {
+			return gimlet.MakeTextErrorResponder(gimlet.ErrorResponse{
+				StatusCode: http.StatusUnauthorized,
+				Message:    fmt.Sprintf("unauthorized to read logs from project '%s'", resourceId),
+			})
+		}
+		return nil
 	}
 
-	bytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return gimlet.MakeJSONInternalErrorResponder(errors.Wrap(err, "error reading response body"))
-	}
-
-	if string(bytes) != "true" {
-		return gimlet.MakeTextErrorResponder(gimlet.ErrorResponse{
-			StatusCode: http.StatusUnauthorized,
-			Message:    fmt.Sprintf("unauthorized to read logs from project '%s'", resourceId),
-		})
-	}
-
-	return nil
+	return responder
 }
