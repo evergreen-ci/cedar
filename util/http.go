c97fa46945e2f0e59b2309531f965d896f57e23f
Sam Kleinman <sam@10gen.com>
2019-12-11 14:38:52 -0500

    MAKE-1061: add retryable client

--- util/http.go
+++ util/http.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"sync"
 	"time"
+
+	"github.com/PuerkitoBio/rehttp"
 )
 
 const httpClientTimeout = 5 * time.Minute
@@ -48,12 +50,84 @@ func GetHTTPClient() *http.Client { return httpClientPool.Get().(*http.Client) }
 func PutHTTPClient(c *http.Client) {
 	c.Timeout = httpClientTimeout
 
-	switch c.Transport.(type) {
+	switch transport := c.Transport.(type) {
 	case *http.Transport:
-		c.Transport.(*http.Transport).TLSClientConfig.InsecureSkipVerify = false
-		httpClientPool.Put(c)
+		transport.TLSClientConfig.InsecureSkipVerify = false
+		c.Transport = transport
+	case *rehttp.Transport:
+		c.Transport = transport.RoundTripper
 	default:
 		c.Transport = newConfiguredBaseTransport()
-		httpClientPool.Put(c)
 	}
+
+	httpClientPool.Put(c)
+}
+
+type HTTPRetryConfiguration struct {
+	MaxDelay        time.Duration
+	BaseDelay       time.Duration
+	MaxRetries      int
+	TemporaryErrors bool
+	Methods         []string
+	Statuses        []int
+}
+
+func NewDefaultHTTPRetryConf() HTTPRetryConfiguration {
+	return HTTPRetryConfiguration{
+		MaxRetries:      100,
+		TemporaryErrors: true,
+		MaxDelay:        5 * time.Second,
+		BaseDelay:       50 * time.Millisecond,
+		Methods: []string{
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodDelete,
+			http.MethodPatch,
+		},
+		Statuses: []int{
+			http.StatusInternalServerError,
+			http.StatusBadGateway,
+			http.StatusServiceUnavailable,
+			http.StatusGatewayTimeout,
+			http.StatusInsufficientStorage,
+			http.StatusConflict,
+			http.StatusRequestTimeout,
+			http.StatusPreconditionFailed,
+		},
+	}
+}
+
+func GetHTTPRetryableClient(conf HTTPRetryConfiguration) *http.Client {
+	client := GetHTTPClient()
+
+	retryFns := []rehttp.RetryFn{}
+
+	if len(conf.Statuses) > 0 {
+		retryFns = append(retryFns, rehttp.RetryStatuses(conf.Statuses...))
+	} else {
+		conf.TemporaryErrors = true
+	}
+
+	if len(conf.Methods) > 0 {
+		retryFns = append(retryFns, rehttp.RetryHTTPMethods(conf.Methods...))
+	}
+
+	if conf.TemporaryErrors {
+		retryFns = append(retryFns, rehttp.RetryTemporaryErr())
+	}
+
+	if conf.MaxRetries > 0 {
+		retryFns = append(retryFns, rehttp.RetryMaxRetries(conf.MaxRetries))
+	}
+
+	client.Transport = rehttp.NewTransport(client.Transport,
+		rehttp.RetryAll(retryFns...),
+		rehttp.ExpJitterDelay(conf.BaseDelay, conf.MaxDelay))
+
+	return client
+}
+
+func GetDefultHTTPRetryableClient() *http.Client {
+	return GetHTTPRetryableClient(NewDefaultHTTPRetryConf())
 }
