13f8c816abcbacdd0c62a079ea52afa8320b276d
Alexander Costas <alexander.costas@mongodb.com>
2020-01-28 12:27:31 -0500

    MAKE-1132

--- model/change_point_detection.go
+++ model/change_point_detection.go
@@ -257,6 +257,12 @@ func GetPerformanceData(ctx context.Context, env cedar.Environment, performanceR
 	if err != nil {
 		return nil, errors.Wrap(err, "Could not decode time series")
 	}
+	if len(res) < 1 {
+		return &PerformanceData{
+			PerformanceResultId: performanceResultId,
+			Data:                []MeasurementData{},
+		}, nil
+	}
 	return &res[0], nil
 }
 
