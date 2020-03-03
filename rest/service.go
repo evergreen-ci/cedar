9cd20f2197d65404940f742550573ac2e9d08110
julianedwards <julian.edwards@mongodb.com>
2020-03-03 15:31:57 -0500

    use different header for evg api key

--- rest/service.go
+++ rest/service.go
@@ -242,7 +242,7 @@ func (s *Service) addRoutes() {
 	s.app.AddRoute("/buildlogger/{id}/meta").Version(1).Get().Wrap(evgAuthReadLogByID).RouteHandler(makeGetLogMetaByID(s.sc))
 	s.app.AddRoute("/buildlogger/task_id/{task_id}").Version(1).Get().Wrap(evgAuthReadLogByTaskID).RouteHandler(makeGetLogByTaskID(s.sc))
 	s.app.AddRoute("/buildlogger/task_id/{task_id}/meta").Version(1).Get().Wrap(evgAuthReadLogByTaskID).RouteHandler(makeGetLogMetaByTaskID(s.sc))
-	s.app.AddRoute("/buildlogger/test_name/{task_id}/{test_name}").Version(1).Wrap(evgAuthReadLogByTaskID).Get().RouteHandler(makeGetLogByTestName(s.sc))
+	s.app.AddRoute("/buildlogger/test_name/{task_id}/{test_name}").Version(1).Get().Wrap(evgAuthReadLogByTaskID).RouteHandler(makeGetLogByTestName(s.sc))
 	s.app.AddRoute("/buildlogger/test_name/{task_id}/{test_name}/meta").Version(1).Get().Wrap(evgAuthReadLogByTaskID).RouteHandler(makeGetLogMetaByTestName(s.sc))
 	s.app.AddRoute("/buildlogger/test_name/{task_id}/{test_name}/group/{group_id}").Version(1).Get().Wrap(evgAuthReadLogByTaskID).RouteHandler(makeGetLogGroup(s.sc))
 }
