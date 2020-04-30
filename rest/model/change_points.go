3371b576a6b6e8a39324693aff7c051480a3919f
Alexander Costas <alexander.costas@mongodb.com>
2020-04-30 11:05:16 -0400

    words

--- rest/model/change_points.go
+++ rest/model/change_points.go
@@ -5,29 +5,29 @@ import dbmodel "github.com/evergreen-ci/cedar/model"
 // APIChangePointGroupedByVersionResult describes a paginated list of changepoints
 // grouped by the version they each belong to.
 type APIChangePointGroupedByVersionResult struct {
-	Versions   []APIChangePointsWithVersion `json:"versions"`
+	Versions   []APIVersionWithChangePoints `json:"versions"`
 	Page       int                          `json:"page"`
 	PageSize   int                          `json:"page_size"`
 	TotalPages int                          `json:"total_pages"`
 }
 
-type APIChangePointsWithVersion struct {
+type APIVersionWithChangePoints struct {
 	VersionId    string                       `json:"version_id"`
 	ChangePoints []APIChangePointWithPerfData `json:"change_points"`
 }
 
 type APIChangePointWithPerfData struct {
-	PerfResultId string `bson:"perf_result_id" json:"perf_result_id"`
-	Project      string `bson:"project" json:"project"`
-	Task         string `bson:"task" json:"task"`
-	Test         string `bson:"test" json:"test"`
-	Variant      string `bson:"variant" json:"variant"`
-	ThreadLevel  int32  `bson:"thread_level" json:"thread_level"`
+	PerfResultId string `json:"perf_result_id"`
+	Project      string `json:"project"`
+	Task         string `json:"task"`
+	Test         string `json:"test"`
+	Variant      string `json:"variant"`
+	ThreadLevel  int32  `json:"thread_level"`
 	dbmodel.ChangePoint
 }
 
 func CreateAPIChangePointGroupedByVersionResult(getChangePointsGroupedByVersionResult []dbmodel.GetChangePointsGroupedByVersionResult, page, pageSize, totalPages int) *APIChangePointGroupedByVersionResult {
-	changePointsGroupedByVersion := make([]APIChangePointsWithVersion, len(getChangePointsGroupedByVersionResult))
+	changePointsGroupedByVersion := make([]APIVersionWithChangePoints, len(getChangePointsGroupedByVersionResult))
 	for i, perfResultsWithVersion := range getChangePointsGroupedByVersionResult {
 		var apiChangePoints []APIChangePointWithPerfData
 		for _, perfResult := range perfResultsWithVersion.PerfResults {
@@ -45,7 +45,7 @@ func CreateAPIChangePointGroupedByVersionResult(getChangePointsGroupedByVersionR
 			}
 		}
 
-		changePointsGroupedByVersion[i] = APIChangePointsWithVersion{
+		changePointsGroupedByVersion[i] = APIVersionWithChangePoints{
 			VersionId:    perfResultsWithVersion.VersionID,
 			ChangePoints: apiChangePoints,
 		}
