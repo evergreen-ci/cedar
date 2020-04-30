455286532ad0296e5e2c9da39cb238dbd6b1fc99
Alexander Costas <alexander.costas@mongodb.com>
2020-04-30 12:50:13 -0400

    graphlookup-fix

--- model/perf_test.go
+++ model/perf_test.go
@@ -656,6 +656,7 @@ func (s *perfResultsSuite) TestFindResultsWithSortAndLimit() {
 
 func (s *perfResultsSuite) TestSearchResultsWithParent() {
 	// nodeA -> nodeB and nodeC, nodeB -> nodeD
+	s.Require().NoError(cedar.GetEnvironment().GetDB().Drop(s.ctx))
 	s.r = new(PerformanceResults)
 	s.r.Setup(cedar.GetEnvironment())
 
@@ -754,9 +755,9 @@ func (s *perfResultsSuite) TestSearchResultsWithParent() {
 	s.NoError(s.r.Find(s.ctx, options))
 	s.Require().Len(s.r.Results, 4)
 	s.Equal(s.r.Results[0].ID, nodeA.ID)
-	s.Equal(s.r.Results[1].ID, nodeD.ID)
+	s.Equal(s.r.Results[3].ID, nodeD.ID)
 	s.Equal(s.r.Results[2].Info.Parent, nodeA.ID)
-	s.Equal(s.r.Results[3].Info.Parent, nodeA.ID)
+	s.Equal(s.r.Results[1].Info.Parent, nodeA.ID)
 
 	// Test min through max depth with $graphLookup
 	options = PerfFindOptions{
@@ -787,9 +788,9 @@ func (s *perfResultsSuite) TestSearchResultsWithParent() {
 	s.NoError(s.r.Find(s.ctx, options))
 	s.Require().Len(s.r.Results, 4)
 	s.Equal(s.r.Results[0].ID, nodeA.ID)
-	s.Equal(s.r.Results[1].ID, nodeD.ID)
-	s.Equal(s.r.Results[2].Info.Parent, nodeA.ID)
+	s.Equal(s.r.Results[2].ID, nodeD.ID)
 	s.Equal(s.r.Results[3].Info.Parent, nodeA.ID)
+	s.Equal(s.r.Results[1].Info.Parent, nodeA.ID)
 
 	// Test tag filtering with $graphLookup
 	options = PerfFindOptions{
