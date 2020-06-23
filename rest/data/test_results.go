1bef68320921f4a192ff9f8630418c5d8e1fcacf
Colin Brown <colin.brown@mongodb.com>
2020-06-23 17:49:05 -0400

    Implement connector interface

--- rest/data/test_results.go
+++ rest/data/test_results.go
@@ -2,44 +2,126 @@ package data
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 
+	dbModel "github.com/evergreen-ci/cedar/model"
 	"github.com/evergreen-ci/cedar/rest/model"
+	"github.com/evergreen-ci/gimlet"
+	"github.com/evergreen-ci/pail"
+	"github.com/mongodb/anser/db"
+	"github.com/pkg/errors"
+	"gopkg.in/mgo.v2/bson"
 )
 
 /////////////////////////////
 // DBConnector Implementation
 /////////////////////////////
 
-func (dbc *DBConnector) FindTestResultByTestName(ctx context.Context, opts TestResultsOptions) (model.APITestResult, error) {
+func (dbc *DBConnector) FindTestResultByTestName(ctx context.Context, opts TestResultsOptions) (*model.APITestResult, error) {
+	dbOpts := dbModel.TestResultsFindOptions{
+		TaskID:         opts.TaskID,
+		Execution:      opts.Execution,
+		EmptyExecution: opts.EmptyExecution,
+	}
 
-	// find by taskid, execution
+	testResults := &dbModel.TestResults{}
+	testResults.Setup(dbc.env)
+	if err := testResults.FindByTaskID(ctx, dbOpts); db.ResultsNotFound(err) {
+		return nil, gimlet.ErrorResponse{
+			StatusCode: http.StatusNotFound,
+			Message:    fmt.Sprintf("tests with task id '%s' not found", opts.TaskID),
+		}
+	} else if err != nil {
+		return nil, gimlet.ErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    fmt.Sprintf("%s", errors.Wrap(err, "retrieving metadata")),
+		}
+	}
 
-	// get bucket
+	bucket, err := testResults.GetBucket(ctx)
+	if err != nil {
+		return nil, gimlet.ErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    fmt.Sprintf("%s", errors.Wrap(err, "getting bucket")),
+		}
+	}
 
-	// get single by key = test name from bucket
-	// return not found error if not
-
-	// download item (look at test_results_iterator)
-
-	// return
-	return model.APITestResult{}, nil
+	return getAPITestResultFromBucket(ctx, bucket, opts.TestName)
 }
 
 ///////////////////////////////
 // MockConnector Implementation
 ///////////////////////////////
 
-func (dbc *MockConnector) FindTestResultByTestName(ctx context.Context, opts TestResultsOptions) (model.APITestResult, error) {
+func (mc *MockConnector) FindTestResultByTestName(ctx context.Context, opts TestResultsOptions) (*model.APITestResult, error) {
+	var testResults *dbModel.TestResults
+	for _, tr := range mc.CachedTestResults {
+		if tr.Info.TaskID == opts.TaskID && (opts.EmptyExecution || tr.Info.Execution == opts.Execution) {
+			testResults = &tr
+		}
+	}
 
-	// find by taskid, execution
+	if testResults == nil {
+		return nil, gimlet.ErrorResponse{
+			StatusCode: http.StatusNotFound,
+			Message:    fmt.Sprintf("tests with task id '%s' not found", opts.TaskID),
+		}
+	}
 
-	// get bucket
+	bucketOpts := pail.LocalOptions{
+		Path:   mc.Bucket,
+		Prefix: testResults.Artifact.Prefix,
+	}
+	bucket, err := pail.NewLocalBucket(bucketOpts)
+	if err != nil {
+		return nil, gimlet.ErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    fmt.Sprintf("%s", errors.Wrap(err, "problem creating bucket")),
+		}
+	}
 
-	// get single by key = test name from bucket
-	// return not found error if not
+	return getAPITestResultFromBucket(ctx, bucket, opts.TestName)
+}
 
-	// download item (look at test_results_iterator)
+func getAPITestResultFromBucket(ctx context.Context, bucket pail.Bucket, testName string) (*model.APITestResult, error) {
+	tr, err := bucket.Get(ctx, testName)
+	if pail.IsKeyNotFoundError(err) {
+		return nil, gimlet.ErrorResponse{
+			StatusCode: http.StatusNotFound,
+			Message:    fmt.Sprintf("test with specified task id and test name not found"),
+		}
+	} else if err != nil {
+		return nil, gimlet.ErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    fmt.Sprintf("%s", errors.Wrap(err, "retrieving test result")),
+		}
+	}
 
-	// return
-	return model.APITestResult{}, nil
+	data, err := ioutil.ReadAll(tr)
+	if err != nil {
+		return nil, gimlet.ErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    fmt.Sprintf("%s", errors.Wrap(err, "reading data")),
+		}
+	}
+
+	var result *dbModel.TestResult
+	if err := bson.Unmarshal(data, result); err != nil {
+		return nil, gimlet.ErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    fmt.Sprintf("%s", errors.Wrap(err, "unmarshalling test result")),
+		}
+	}
+
+	var apiResult *model.APITestResult
+	if err := apiResult.Import(result); err != nil {
+		return nil, gimlet.ErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    fmt.Sprintf(fmt.Sprintf("%s", errors.Wrap(err, "converting test result to output format"))),
+		}
+	}
+
+	return apiResult, nil
 }
