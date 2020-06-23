4fd0cd6cbe0b7fa0a6ca530f2e257efb93c8cfe4
Maya Ravichandran <maya.ravichandran@Mayas-MacBook-Pro.local>
2020-06-23 11:10:18 -0400

    WIP implementing rest routes

--- rest/data/test_results.go
+++ rest/data/test_results.go
@@ -4,17 +4,10 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"sort"
-	"strings"
 
 	"github.com/evergreen-ci/cedar/model"
 	dataModel "github.com/evergreen-ci/cedar/rest/model"
-	"github.com/evergreen-ci/cedar/units"
 	"github.com/evergreen-ci/gimlet"
-	"github.com/mongodb/amboy"
-	"github.com/mongodb/grip"
-	"github.com/mongodb/grip/message"
-	"github.com/pkg/errors"
 )
 
 /////////////////////////////
@@ -23,25 +16,68 @@ import (
 
 // FindTestResultsByTaskId queries the database to find the test
 // result with the given id.
-func (dbc *DBConnector) FindTestResultsByTaskId(ctx context.Context, id string) (*dataModel.APITestResult, error) {
-	result := model.TestResult{}
-	result.Setup(dbc.env)
-	result.ID = id
+// func (dbc *DBConnector) FindTestResultsByTaskId(ctx context.Context, id string) (*dataModel.APIPerformanceResult, error) {
+// 	results := model.TestResult{}
+// 	results.Setup(dbc.env)
+// 	results.ID = id
 
-	if err := result.Find(ctx); err != nil {
-		return nil, gimlet.ErrorResponse{
-			StatusCode: http.StatusNotFound,
-			Message:    fmt.Sprintf("test result with id '%s' not found", id),
-		}
+// 	if err := result.Find(ctx); err != nil {
+// 		return nil, gimlet.ErrorResponse{
+// 			StatusCode: http.StatusNotFound,
+// 			Message:    fmt.Sprintf("test result with id '%s' not found", id),
+// 		}
+// 	}
+
+// 	apiResult := dataModel.APIPerformanceResult{}
+// 	err := apiResult.Import(result)
+// 	if err != nil {
+// 		return nil, gimlet.ErrorResponse{
+// 			StatusCode: http.StatusInternalServerError,
+// 			Message:    fmt.Sprintf("corrupt data"),
+// 		}
+// 	}
+// 	return &apiResult, nil
+// }
+
+
+// FindTestResultsByTaskId queries the database to find all performance
+// results with the given taskId and that fall within interval, filtered by
+// tags.
+func (dbc *DBConnector) FindTestResultsByTaskId(ctx context.Context, taskId string, interval model.TimeRange, tags ...string) ([]dataModel.APIPerformanceResult, error) {
+	results := model.TestResults{}
+	results.Setup(dbc.env)
+
+	options := model.TestResultsFindOptions{
+		Interval: interval,
+		Info: model.TestResultInfo{
+			TaskID: taskId,
+			Tags:   tags,
+		},
+		MaxDepth: 0,
 	}
 
-	apiResult := dataModel.APITestResult{}
-	err := apiResult.Import(result)
-	if err != nil {
+	if err := results.Find(ctx, options); err != nil {
 		return nil, gimlet.ErrorResponse{
 			StatusCode: http.StatusInternalServerError,
-			Message:    fmt.Sprintf("corrupt data"),
+			Message:    fmt.Sprintf("database error"),
 		}
 	}
-	return &apiResult, nil
+	if results.IsNil() {
+		return nil, gimlet.ErrorResponse{
+			StatusCode: http.StatusNotFound,
+			Message:    fmt.Sprintf("test results with task_id '%s' not found", taskId),
+		}
+	}
+
+	apiResults := make([]dataModel.APITestResult, len(results.Results))
+	for i, result := range results.Results {
+		err := apiResults[i].Import(result)
+		if err != nil {
+			return nil, gimlet.ErrorResponse{
+				StatusCode: http.StatusInternalServerError,
+				Message:    fmt.Sprintf("corrupt data"),
+			}
+		}
+	}
+	return apiResults, nil
 }
