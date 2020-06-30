77b15fb29899acdb9e09742792d3d5e3f07594fa
Colin Brown <colin.brown@mongodb.com>
2020-06-30 12:55:33 -0400

    Initial changes

--- model/system_metrics.go
+++ model/system_metrics.go
@@ -1,15 +1,24 @@
 package model
 
 import (
+	"context"
 	"crypto/sha1"
 	"fmt"
 	"hash"
 	"io"
+	"testing"
 	"time"
 
 	"github.com/evergreen-ci/cedar"
 	"github.com/mongodb/anser/bsonutil"
+	"github.com/mongodb/anser/db"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
 )
+
+const systemMetricsCollection = "system_metrics"
 
 // SystemMetrics describes metadata for the system metrics data for
 // a given task execution.
@@ -31,6 +40,76 @@ var (
 	systemMetricsCompletedAtKey = bsonutil.MustHaveTag(SystemMetrics{}, "CompletedAt")
 	systemMetricsArtifactKey    = bsonutil.MustHaveTag(SystemMetrics{}, "Artifact")
 )
+
+// Find searches the database for the system metrics object. The environment should
+// not be nil. Either the ID or full Info of the system metrics object needs to be
+// specified.
+func (result *SystemMetrics) Find(ctx context.Context) error {
+	if result.env == nil {
+		return errors.New("cannot find with a nil environment")
+	}
+
+	if result.ID == "" {
+		result.ID = result.Info.ID()
+	}
+
+	result.populated = false
+	err := result.env.GetDB().Collection(systemMetricsCollection).FindOne(ctx, bson.M{"_id": result.ID}).Decode(result)
+	if db.ResultsNotFound(err) {
+		return errors.New("could not find system metrics record in the database")
+	} else if err != nil {
+		return errors.Wrap(err, "problem finding system metrics")
+	}
+
+	result.populated = true
+
+	return nil
+}
+
+func TestSystemMetricsFind(t *testing.T) {
+	env := cedar.GetEnvironment()
+	db := env.GetDB()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	defer func() {
+		assert.NoError(t, db.Collection(systemMetricsCollection).Drop(ctx))
+	}()
+
+	sm1 := getSystemMetrics()
+	_, err := db.Collection(systemMetricsCollection).InsertOne(ctx, sm1)
+	require.NoError(t, err)
+	sm2 := getSystemMetrics()
+	_, err = db.Collection(systemMetricsCollection).InsertOne(ctx, sm2)
+	require.NoError(t, err)
+
+	t.Run("DNE", func(t *testing.T) {
+		sm := SystemMetrics{ID: "DNE"}
+		sm.Setup(env)
+		assert.Error(t, sm.Find(ctx))
+	})
+	t.Run("NoEnv", func(t *testing.T) {
+		sm := SystemMetrics{ID: sm1.ID}
+		assert.Error(t, sm.Find(ctx))
+	})
+	t.Run("WithID", func(t *testing.T) {
+		sm := SystemMetrics{ID: sm1.ID}
+		sm.Setup(env)
+		require.NoError(t, sm.Find(ctx))
+		assert.Equal(t, sm1.ID, sm.ID)
+		assert.Equal(t, sm1.Info, sm.Info)
+		assert.Equal(t, sm1.Artifact, sm.Artifact)
+		assert.True(t, sm.populated)
+	})
+	t.Run("WithoutID", func(t *testing.T) {
+		sm := SystemMetrics{Info: sm2.Info}
+		sm.Setup(env)
+		require.NoError(t, sm.Find(ctx))
+		assert.Equal(t, sm2.ID, sm.ID)
+		assert.Equal(t, sm2.Info, sm.Info)
+		assert.Equal(t, sm2.Artifact, sm.Artifact)
+		assert.True(t, sm.populated)
+	})
+}
 
 // SystemMetricsInfo describes information unique to the system metrics for a task.
 type SystemMetricsInfo struct {
