6ec292c2675a1600078b9988b1ed4e283735b52a
Maya Ravichandran <maya.ravichandran@Mayas-MacBook-Pro.local>
2020-07-02 13:21:36 -0400

    EVG-8979: made naming more consistent

--- model/system_metrics.go
+++ model/system_metrics.go
@@ -114,30 +114,30 @@ func (sm *SystemMetrics) Find(ctx context.Context) error {
 	return nil
 }
 
-// SaveNew saves a new system metrics result to the database. If a result with the
-// same ID already exists an error is returned. The result should be populated
+// SaveNew saves a new system metrics record to the database. If a record with the
+// same ID already exists an error is returned. The record should be populated
 // and the environment should not be nil.
-func (result *SystemMetrics) SaveNew(ctx context.Context) error {
-	if !result.populated {
-		return errors.New("cannot save unpopulated system metrics result")
+func (sm *SystemMetrics) SaveNew(ctx context.Context) error {
+	if !sm.populated {
+		return errors.New("cannot save unpopulated system metrics record")
 	}
-	if result.env == nil {
+	if sm.env == nil {
 		return errors.New("cannot save with a nil environment")
 	}
 
-	if result.ID == "" {
-		result.ID = result.Info.ID()
+	if sm.ID == "" {
+		sm.ID = sm.Info.ID()
 	}
 
-	insertResult, err := result.env.GetDB().Collection(systemMetricsCollection).InsertOne(ctx, result)
+	insertResult, err := sm.env.GetDB().Collection(systemMetricsCollection).InsertOne(ctx, sm)
 	grip.DebugWhen(err == nil, message.Fields{
 		"collection":   systemMetricsCollection,
-		"id":           result.ID,
+		"id":           sm.ID,
 		"insertResult": insertResult,
-		"op":           "save new system metrics result",
+		"op":           "save new system metrics record",
 	})
 
-	return errors.Wrapf(err, "problem saving new system metrics result %s", result.ID)
+	return errors.Wrapf(err, "problem saving new system metrics record %s", sm.ID)
 }
 
 // Append uploads a chunk of system metrics data to the offline blob storage bucket
