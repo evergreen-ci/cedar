b88790409e3e1157ae6b37be8e1a192fcf1b2d0e
julianedwards <julian.edwards@mongodb.com>
2020-08-12 16:18:55 -0400

    drop db before test

--- model/system_metrics_test.go
+++ model/system_metrics_test.go
@@ -86,6 +86,7 @@ func TestSystemMetricsFindByTaskID(t *testing.T) {
 	db := env.GetDB()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+	assert.NoError(t, db.Collection(systemMetricsCollection).Drop(ctx))
 	defer func() {
 		assert.NoError(t, db.Collection(systemMetricsCollection).Drop(ctx))
 	}()
