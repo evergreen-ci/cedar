bb78909e36ab6e05a51862a3f5c89d44229f212c
Kim Tao <kim.tao@mongodb.com>
2020-08-27 11:33:59 -0400

    EVG-12721: add debug logs for system metrics (#278)

--- rpc/internal/system_metrics_service.go
+++ rpc/internal/system_metrics_service.go
@@ -8,6 +8,8 @@ import (
 	"github.com/evergreen-ci/cedar"
 	"github.com/evergreen-ci/cedar/model"
 	"github.com/mongodb/anser/db"
+	"github.com/mongodb/grip"
+	"github.com/mongodb/grip/message"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
@@ -48,14 +50,36 @@ func (s *systemMetricsService) AddSystemMetrics(ctx context.Context, data *Syste
 	systemMetrics := &model.SystemMetrics{ID: data.Id}
 	systemMetrics.Setup(s.env)
 	if err := systemMetrics.Find(ctx); err != nil {
+		grip.Error(message.WrapError(err, message.Fields{
+			"message":    "could not find system metrics",
+			"chunk_id":   data.Id,
+			"chunk_type": data.Type,
+			"op":         "adding system metrics",
+			"context":    "system metrics gRPC service",
+		}))
 		if db.ResultsNotFound(err) {
 			return nil, newRPCError(codes.NotFound, err)
 		}
 		return nil, newRPCError(codes.Internal, errors.Wrapf(err, "problem finding systemMetrics record for '%s'", data.Id))
 	}
 
+	err := systemMetrics.Append(ctx, data.Type, data.Format.Export(), data.Data)
+	grip.Error(message.WrapError(err, message.Fields{
+		"message":    "error appending system metrics",
+		"chunk_id":   data.Id,
+		"chunk_type": data.Type,
+		"op":         "adding system metrics",
+		"context":    "system metrics gRPC service",
+	}))
+	grip.InfoWhen(err == nil, message.Fields{
+		"message":    "successfully added system metrics",
+		"chunk_id":   data.Id,
+		"chunk_type": data.Type,
+		"op":         "adding system metrics",
+		"context":    "system metrics gRPC service",
+	})
 	return &SystemMetricsResponse{Id: systemMetrics.ID},
-		newRPCError(codes.Internal, errors.Wrapf(systemMetrics.Append(ctx, data.Type, data.Format.Export(), data.Data), "problem appending systemMetrics data for '%s'", data.Id))
+		newRPCError(codes.Internal, errors.Wrapf(err, "problem appending systemMetrics data for '%s'", data.Id))
 }
 
 // StreamSystemMetrics adds system metrics data via client-side streaming to an existing
@@ -70,13 +94,30 @@ func (s *systemMetricsService) StreamSystemMetrics(stream CedarSystemMetrics_Str
 		}
 
 		chunk, err := stream.Recv()
+		grip.Error(message.WrapError(err, message.Fields{
+			"message": "error while receiving chunk",
+			"op":      "streaming system metrics",
+			"context": "system metrics gRPC service",
+		}))
 		if err == io.EOF {
+			grip.Info(message.Fields{
+				"message": "successfully finished streaming system metrics",
+				"op":      "adding system metrics",
+				"context": "system metrics gRPC service",
+			})
 			return stream.SendAndClose(&SystemMetricsResponse{Id: id})
 		}
 		if err != nil {
 			return newRPCError(codes.Internal, errors.Wrapf(err, "error in stream for '%s'", id))
 		}
 
+		grip.Info(message.Fields{
+			"message":    "successfully received chunk",
+			"chunk_id":   chunk.Id,
+			"chunk_type": chunk.Type,
+			"op":         "streaming system metrics",
+			"context":    "system metrics gRPC service",
+		})
 		if id == "" {
 			id = chunk.Id
 		} else if chunk.Id != id {
@@ -84,9 +125,23 @@ func (s *systemMetricsService) StreamSystemMetrics(stream CedarSystemMetrics_Str
 		}
 
 		_, err = s.AddSystemMetrics(ctx, chunk)
+		grip.Error(message.WrapError(err, message.Fields{
+			"message":    "error while adding system metrics",
+			"chunk_id":   chunk.Id,
+			"chunk_type": chunk.Type,
+			"op":         "streaming system metrics",
+			"context":    "system metrics gRPC service",
+		}))
 		if err != nil {
 			return newRPCError(codes.Internal, errors.Wrapf(err, "error in adding data for '%s'", id))
 		}
+		grip.Info(message.Fields{
+			"message":    "successfully added system metrics to stream",
+			"chunk_id":   chunk.Id,
+			"chunk_type": chunk.Type,
+			"op":         "adding system metrics",
+			"context":    "system metrics gRPC service",
+		})
 	}
 }
 
@@ -102,6 +157,12 @@ func (s *systemMetricsService) CloseMetrics(ctx context.Context, info *SystemMet
 		}
 		return nil, newRPCError(codes.Internal, errors.Wrapf(err, "problem finding system metrics record for '%s'", info.Id))
 	}
+	err := systemMetrics.Close(ctx, info.Success)
+	grip.InfoWhen(err == nil, message.Fields{
+		"message": "successfully finished streaming system metrics",
+		"op":      "adding system metrics",
+		"context": "system metrics gRPC service",
+	})
 	return &SystemMetricsResponse{Id: systemMetrics.ID},
-		newRPCError(codes.Internal, errors.Wrapf(systemMetrics.Close(ctx, info.Success), "problem closing system metrics record with id %s", systemMetrics.ID))
+		newRPCError(codes.Internal, errors.Wrapf(err, "problem closing system metrics record with id %s", systemMetrics.ID))
 }
