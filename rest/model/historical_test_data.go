64d91f818d1394581ad3b0bf73d2128c5d8482f8
julianedwards <julian.edwards@mongodb.com>
2021-01-25 11:21:37 -0500

    add APIAggregatedHistoricalTestData

--- rest/model/historical_test_data.go
+++ rest/model/historical_test_data.go
@@ -5,22 +5,29 @@ import (
 	"github.com/pkg/errors"
 )
 
-// APIHistoricalTestData describes aggregated test result data for a given date
-// range.
-type APIHistoricalTestData struct {
-	Info            APIHistoricalTestDataInfo `json:"info"`
-	NumPass         int                       `json:"num_pass"`
-	NumFail         int                       `json:"num_fail"`
-	AverageDuration float64                   `json:"average_duration"`
-	LastUpdate      APITime                   `json:"last_update"`
+// APIAggregatedHistoricalTestData describes aggregated test result data for a
+// given date range.
+type APIAggregatedHistoricalTestData struct {
+	TestName APIString `json:"test_name"`
+	TaskName APIString `json:"task_name,omitempty"`
+	Variant  APIString `json:"variant,omitempty"`
+	Date     APITime   `json:"date"`
+
+	NumPass         int     `json:"num_pass"`
+	NumFail         int     `json:"num_fail"`
+	AverageDuration float64 `json:"average_duration"`
+	LastUpdate      APITime `json:"last_update"`
 }
 
-// Import transforms a HistoricalTestData object into an APIHistoricalTestData
-// object.
-func (a *APIHistoricalTestData) Import(i interface{}) error {
+// Import transforms an AggregatedHistoricalTestData object into an
+// APIAggregatedHistoricalTestData object.
+func (a *APIAggregatedHistoricalTestData) Import(i interface{}) error {
 	switch hd := i.(type) {
-	case dbmodel.HistoricalTestData:
-		a.Info = getHistoricalTestDataInfo(hd.Info)
+	case dbmodel.AggregatedHistoricalTestData:
+		a.TestName = ToAPIString(hd.TestName)
+		a.TaskName = ToAPIString(hd.TaskName)
+		a.Variant = ToAPIString(hd.Variant)
+		a.Date = NewTime(hd.Date)
 		a.NumPass = hd.NumPass
 		a.NumFail = hd.NumFail
 		a.AverageDuration = hd.AverageDuration.Seconds()
@@ -29,26 +36,4 @@ func (a *APIHistoricalTestData) Import(i interface{}) error {
 		return errors.New("incorrect type when converting to APIHistoricalTestData type")
 	}
 	return nil
-}
-
-// APIHistoricalTestDataInfo describes information unique to a single test
-// statistics document.
-type APIHistoricalTestDataInfo struct {
-	Project     APIString `json:"project"`
-	Variant     APIString `json:"variant"`
-	TaskName    APIString `json:"task_name"`
-	TestName    APIString `json:"test_name"`
-	RequestType APIString `json:"request_type"`
-	Date        APITime   `json:"date"`
-}
-
-func getHistoricalTestDataInfo(info dbmodel.HistoricalTestDataInfo) APIHistoricalTestDataInfo {
-	return APIHistoricalTestDataInfo{
-		Project:     ToAPIString(info.Project),
-		Variant:     ToAPIString(info.Variant),
-		TaskName:    ToAPIString(info.TaskName),
-		TestName:    ToAPIString(info.TestName),
-		RequestType: ToAPIString(info.RequestType),
-		Date:        NewTime(info.Date),
-	}
 }
--- rest/model/historical_test_data_test.go
+++ rest/model/historical_test_data_test.go
@@ -10,40 +10,32 @@ import (
 
 func TestHistoricalTestDataImport(t *testing.T) {
 	t.Run("InvalidType", func(t *testing.T) {
-		apiHistoricalTestData := &APIHistoricalTestData{}
-		assert.Error(t, apiHistoricalTestData.Import(dbmodel.TestResults{}))
+		api := &APIAggregatedHistoricalTestData{}
+		assert.Error(t, api.Import(dbmodel.TestResults{}))
 	})
 	t.Run("ValidHistoricalTestData", func(t *testing.T) {
-		tr := dbmodel.HistoricalTestData{
-			Info: dbmodel.HistoricalTestDataInfo{
-				Project:     "project",
-				Variant:     "variant",
-				TaskName:    "task_name",
-				TestName:    "test_name",
-				RequestType: "request_type",
-				Date:        time.Now(),
-			},
+		tr := dbmodel.AggregatedHistoricalTestData{
+			TestName:        "test_name",
+			TaskName:        "task_name",
+			Variant:         "variant",
+			Date:            time.Now(),
 			NumPass:         2,
 			NumFail:         2,
 			AverageDuration: 30 * time.Second,
 			LastUpdate:      time.Now(),
 		}
-		expected := &APIHistoricalTestData{
-			Info: APIHistoricalTestDataInfo{
-				Project:     ToAPIString(tr.Info.Project),
-				Variant:     ToAPIString(tr.Info.Variant),
-				TaskName:    ToAPIString(tr.Info.TaskName),
-				TestName:    ToAPIString(tr.Info.TestName),
-				RequestType: ToAPIString(tr.Info.RequestType),
-				Date:        NewTime(tr.Info.Date),
-			},
+		expected := &APIAggregatedHistoricalTestData{
+			TestName:        ToAPIString(tr.TestName),
+			TaskName:        ToAPIString(tr.TaskName),
+			Variant:         ToAPIString(tr.Variant),
+			Date:            NewTime(tr.Date),
 			NumPass:         tr.NumPass,
 			NumFail:         tr.NumFail,
 			AverageDuration: tr.AverageDuration.Seconds(),
 			LastUpdate:      NewTime(tr.LastUpdate),
 		}
-		apiHistoricalTestData := &APIHistoricalTestData{}
-		assert.NoError(t, apiHistoricalTestData.Import(tr))
-		assert.Equal(t, expected, apiHistoricalTestData)
+		api := &APIAggregatedHistoricalTestData{}
+		assert.NoError(t, api.Import(tr))
+		assert.Equal(t, expected, api)
 	})
 }
