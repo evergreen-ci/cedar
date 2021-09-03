d505791a67c2f08aea493ce4338c881131096f2d
Jonathan Brill <jonathan.brill@10gen.com>
2021-09-03 15:50:09 -0400

    EVG-15255 new update time series job (#380)

--- rpc/internal/perf_service.go
+++ rpc/internal/perf_service.go
@@ -56,6 +56,14 @@ func (srv *perfService) CreateMetricSeries(ctx context.Context, result *ResultDa
 	if err := record.SaveNew(ctx); err != nil {
 		return resp, newRPCError(codes.Internal, errors.Wrap(err, "problem saving record"))
 	}
+	if len(record.Rollups.Stats) > 0 {
+		processingJob := units.NewUpdateTimeSeriesJob(record.Info.ToPerformanceResultSeriesID())
+		err := amboy.EnqueueUniqueJob(ctx, srv.env.GetRemoteQueue(), processingJob)
+
+		if err != nil {
+			return nil, newRPCError(codes.Internal, errors.Wrapf(err, "problem creating signal processing job for perf result '%s'", record.ID))
+		}
+	}
 
 	if err := srv.addFTDCRollupsJob(ctx, record.ID, record.Artifacts); err != nil {
 		return resp, errors.Wrap(err, "problem creating ftdc rollups job")
