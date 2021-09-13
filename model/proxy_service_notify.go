bfbaf69ebf2a36bf223d712e59a3222e9651c53c
anjani.bhat <anjani.bhat@mongodb.com>
2021-09-13 16:09:11 -0400

    EVG-14674: Fix model

--- model/proxy_service_notify.go
+++ model/proxy_service_notify.go
@@ -2,13 +2,13 @@ package model
 
 // PerformanceTestResultID is the ID to uniquely identify a new performance result available at test level.
 type PerformanceTestResultID struct {
-	Version   string           `bson:"version"`
-	TaskID    string           `bson:"task_id"`
-	Project   string           `bson:"project"`
-	Variant   string           `bson:"variant"`
-	Task      string           `bson:"task"`
-	Test      string           `bson:"test"`
-	Arguments map[string]int32 `bson:"args"`
+	Version   string           `json:"version"`
+	TaskID    string           `json:"task_id"`
+	Project   string           `json:"project"`
+	Variant   string           `json:"variant"`
+	Task      string           `json:"task"`
+	Test      string           `json:"test"`
+	Arguments map[string]int32 `json:"args"`
 }
 
 type PerformanceAnalysisProxyServiceOptions struct {
