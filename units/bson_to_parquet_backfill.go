49b9f7bd211802cbd63e5f3b0077e336897ecb19
julianedwards <julian.edwards@mongodb.com>
2022-03-02 13:52:43 -0500

    initial iteration of backfill job

---
+++ units/bson_to_parquet_backfill.go
@@ -0,0 +1,178 @@
+package units
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/evergreen-ci/cedar"
+	"github.com/evergreen-ci/cedar/model"
+	"github.com/mongodb/amboy"
+	"github.com/mongodb/amboy/job"
+	"github.com/mongodb/amboy/registry"
+	"github.com/mongodb/anser/bsonutil"
+	"github.com/mongodb/grip"
+	"github.com/mongodb/grip/message"
+	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const bsonToParquetBackfillJobName = "bson-to-parquet-backfill"
+
+type bsonToParquetBackfillJob struct {
+	job.Base `bson:"metadata" json:"metadata" yaml:"metadata"`
+
+	env         cedar.Environment
+	queue       amboy.Queue
+	controller  *model.BatchJobController
+	migrationID string
+}
+
+func init() {
+	registry.AddJobType(bsonToParquetBackfillJobName, func() amboy.Job { return makeBSONToParquetBackfillJob() })
+}
+
+func makeBSONToParquetBackfillJob() *bsonToParquetBackfillJob {
+	return &bsonToParquetBackfillJob{
+		Base: job.Base{
+			JobType: amboy.JobType{
+				Name:    findOutdatedRollupsJobName,
+				Version: 1,
+			},
+		},
+	}
+}
+
+func NewBSONToParquetBackfillJob(id string) amboy.Job {
+	j := makeBSONToParquetBackfillJob()
+	j.SetID(fmt.Sprintf("%s.%s", bsonToParquetBackfillJobName, id))
+	j.SetEnqueueAllScopes(true)
+
+	return j
+}
+
+func (j *bsonToParquetBackfillJob) Run(ctx context.Context) {
+	defer j.MarkComplete()
+
+	if j.env == nil {
+		j.env = cedar.GetEnvironment()
+	}
+	if j.queue == nil {
+		j.queue = j.env.GetRemoteQueue()
+	}
+
+	controller, err := model.FindBatchJobController(ctx, j.env, bsonToParquetBackfillJobName)
+	if err != nil {
+		j.AddError(errors.Wrap(err, "getting batch job controller"))
+		return
+	}
+	if controller.Iterations <= 0 {
+		controller.Iterations = 1
+	}
+	j.controller = controller
+
+	if j.controller.Timeout > 0 {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, j.controller.Timeout)
+		defer cancel()
+	}
+
+	for i := 0; i < j.controller.Iterations; i++ {
+		if err = j.runIteration(ctx); err != nil {
+			j.AddError(err)
+			return
+		}
+	}
+}
+
+func (j *bsonToParquetBackfillJob) runIteration(ctx context.Context) error {
+	start := time.Now().UTC()
+	collection := j.env.GetDB().Collection(j.controller.Collection)
+
+	// This is really ugly, but unfortunately Mongo does not allow setting
+	// limits on UpdateMany operations so we have to do a BulkWrite with
+	// the batch size number of UpdateOne operations.
+	updates := make([]mongo.WriteModel, j.controller.BatchSize)
+	for i := 0; i < j.controller.BatchSize; i++ {
+		updates[i] = mongo.NewUpdateOneModel().
+			SetFilter(bson.M{"$or": []bson.M{
+				bson.M{model.TestResultsMigrationKey: bson.M{"$exists": false}},
+				bson.M{bsonutil.GetDottedKeyName(model.TestResultsMigrationKey, model.MigrationStatsVersionKey): bson.M{"$lt": j.controller.Version}},
+				bson.M{"$and": []bson.M{
+					bson.M{bsonutil.GetDottedKeyName(model.TestResultsMigrationKey, model.MigrationStatsStartedAtKey): bson.M{"$and": []bson.M{
+						bson.M{"gt": time.Time{}},
+						bson.M{"lt": time.Now().Add(-j.controller.Timeout)},
+					}}},
+					bson.M{bsonutil.GetDottedKeyName(model.TestResultsMigrationKey, model.MigrationStatsCompletedAtKey): time.Time{}},
+				}},
+			}}).
+			SetUpdate(bson.M{"$set": bson.M{
+				model.TestResultsMigrationKey: &model.MigrationStats{
+					MigratorID: j.ID(),
+					StartedAt:  start,
+					Version:    j.controller.Version,
+				},
+			}})
+	}
+	bulkWriteResult, err := collection.BulkWrite(ctx, updates)
+	if err != nil {
+		return errors.Wrap(err, "marking test results for migration in temporary collection")
+	}
+	if bulkWriteResult.MatchedCount == 0 {
+		grip.Info(message.Fields{
+			"job_name": bsonToParquetBackfillJobName,
+			"message":  "test results BSON to Parquet backfill complete",
+		})
+		return nil
+	}
+
+	cur, err := collection.Find(
+		ctx,
+		bson.M{
+			bsonutil.GetDottedKeyName(model.TestResultsMigrationKey, model.MigrationStatsMigratorIDKey):  j.ID(),
+			bsonutil.GetDottedKeyName(model.TestResultsMigrationKey, model.MigrationStatsCompletedAtKey): time.Time{},
+		},
+		options.Find().SetLimit(int64(j.controller.BatchSize)),
+	)
+	if err != nil {
+		return errors.Wrap(err, "getting test results from temporary collection")
+	}
+	defer func() {
+		j.AddError(errors.Wrap(cur.Close(ctx), "closing DB cursor"))
+	}()
+
+	var results []model.TestResults
+	if err = cur.All(ctx, &results); err != nil {
+		return errors.Wrap(err, "decoding test results from temporary collection")
+	}
+
+	toUpdate := make([]string, len(results))
+	for i, result := range results {
+		if err = result.DownloadConvertAndWriteParquet(ctx); err != nil {
+			return errors.Wrap(err, "downloading, converting, and writing Parquet test results")
+		}
+
+		toUpdate[i] = result.ID
+	}
+
+	// TODO: Should this be a bulk update? Or should we update each
+	// individual test result doc after a successful write to the Presto
+	// bucket above?
+	updateResult, err := collection.UpdateMany(
+		ctx,
+		bson.M{"_id": bson.M{"$in": toUpdate}},
+		bson.M{"$set": bson.M{
+			bsonutil.GetDottedKeyName(model.TestResultsMigrationKey, model.MigrationStatsCompletedAtKey): time.Now(),
+		}},
+	)
+	if err != nil {
+		return errors.Wrap(err, "marking migrated test results complete in temporary collection")
+	}
+	if updateResult.ModifiedCount != int64(len(toUpdate)) {
+		return errors.Errorf("expected to mark complete %d migrated test results documents from temporary collection, but marked %d instead", len(toUpdate), updateResult.ModifiedCount)
+	}
+
+	return nil
+}
